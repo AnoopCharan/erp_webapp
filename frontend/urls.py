4cde50537923a8d123e9f4eb19378a0e26be6e02
Anoop <nakkasaicharan@gmail.com>
2022-05-07 17:54:08 -0700

    current stock and min stock pages added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-from frontend.views import homePage, logoutRedirect
+from frontend import views
 
 
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', homePage.as_view(), name='home'),
-    path('logout', logoutRedirect, name='logoutRedirect'),
+    path('', views.HomePage.as_view(), name='home'),
+    path('current', views.CurrentStock.as_view(), name='currentHome'),
+    path('min', views.MinimumStock.as_view(), name='minHome'),
+    path('logout', views.logoutRedirect, name='logoutRedirect'),
     path('test',(TemplateView.as_view(template_name='registration/login_test.html')), name='test')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,4 @@
-from msilib import type_binary
+
 from urllib import response
 from django.urls import reverse, resolve, reverse_lazy
 # from django.core.urlresolvers import reverse_lazy
@@ -7,60 +7,76 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect, HttpResponse
-from utils.apiUtils import get_token
+from utils.apiUtils import api_get, api_auth
 import requests
 from rest_framework import status
 
 
 # Create your views here.
 
+
+
 # Homepage
 
-class homePage(LoginRequiredMixin, View):
+class HomePage(LoginRequiredMixin, View):
     # redirect_field_name = 'redirect_to'
     template_name = 'home/dash.html'
     dashEndpoint = reverse_lazy('api:dash-list')
     typeEndpoint = reverse_lazy('api:partcategory-list')
-    
 
-    
     def get(self,request):
 
-        apiHeader = get_token(user= request.user, asHeader=True)
-             
-        # Get dash endpoint data
-        try:
-            dashGet = requests.get(url=request.build_absolute_uri(self.dashEndpoint), headers=apiHeader)
-            dashGet.raise_for_status()
-            
+        dashUrl = request.build_absolute_uri(self.dashEndpoint)
+        dashGet = api_get(url= dashUrl, request= request)
+        print(dashGet.status_code)
 
-        except requests.exceptions.HTTPError as e:
-            return HttpResponse(content=f"""<h1>Something went wrong</h1>
-                                            <h2>CODE: {dashGet.status_code}  {dashGet.reason}</h2>
-                                            <h2>{e.response.text}</h2>
-                                            """)
-        
-        # Get types endpoint data
-        try:
-            typeGet = requests.get(url=request.build_absolute_uri(self.typeEndpoint), headers=apiHeader)
-            typeGet.raise_for_status()
-            
-
-        except requests.exceptions.HTTPError as e:
-            return HttpResponse(content=f"""<h1>Something went wrong</h1>
-                                            <h2>CODE: {typeGet.status_code}  {typeGet.reason}</h2>
-                                            <h2>{e.response.text}</h2>
-                                            """)
+        typeUrl = request.build_absolute_uri(self.typeEndpoint)
+        typeGet = api_get(url=typeUrl, request=request)
 
         data ={'dash':dashGet.json(), 'types':typeGet.json(), 'module':'Dashboard'}
         
         return render(request, template_name=self.template_name, context=data)
 
+class CurrentStock(LoginRequiredMixin, View):
+    template_name = 'current/current.html'
+    dashEndpoint = reverse_lazy('api:dash-list')
+    typeEndpoint = reverse_lazy('api:partcategory-list')
 
+    def get(self,request):
+
+        dashUrl = request.build_absolute_uri(self.dashEndpoint)
+        dashGet = api_get(url= dashUrl, request= request)
+        print(dashGet.status_code)
+
+        typeUrl = request.build_absolute_uri(self.typeEndpoint)
+        typeGet = api_get(url=typeUrl, request=request)
+
+        data ={'dash':dashGet.json(), 'types':typeGet.json(), 'module':'Current Stock'}
+        
+        return render(request, template_name=self.template_name, context=data)
+
+class MinimumStock(LoginRequiredMixin, View):
+    template_name = 'min/min.html'
+    dashEndpoint = reverse_lazy('api:dash-list')
+    typeEndpoint = reverse_lazy('api:partcategory-list')
+
+    def get(self,request):
+
+        dashUrl = request.build_absolute_uri(self.dashEndpoint)
+        dashGet = api_get(url= dashUrl, request= request)
+        print(dashGet.status_code)
+
+        typeUrl = request.build_absolute_uri(self.typeEndpoint)
+        typeGet = api_get(url=typeUrl, request=request)
+
+
+        data ={'dash':dashGet.json(), 'types':typeGet.json(), 'module':'Minimum Stock'}
+        
+        return render(request, template_name=self.template_name, context=data)
 
 
 def logoutRedirect(request):
     # print(request)
     logout(request=request)
     
-    return HttpResponseRedirect(redirect_to=reverse('home:home'))
+    return HttpResponseRedirect(redirect_to=reverse('frontend:home'))
