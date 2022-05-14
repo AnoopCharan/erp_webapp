07199856ff2a3a864127171c1015e28cfccf51fa
Anoop <nakkasaicharan@gmail.com>
2022-05-13 18:44:15 -0700

    first deployment

--- erpWa/settings.py
+++ erpWa/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +108,9 @@ DATABASES = {
 
     }
 }
-
+# For HEroku
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -143,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -193,3 +199,4 @@ REST_FRAMEWORK = {
 # Media URL
 MEDIA_URL = "/media/"
 
+django_heroku.settings(locals())
